46606607ae0f391a7cf2c0d8e8c679465b3337a4
UmairPervaiz <umairpervaizbutt@gmail.com>
2024-09-18 21:44:47 +0500

    Updated for deployment

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '../assets')));
 
+app.use(express.static(
+  path.resolve(__dirname, '../dist'),
+  { maxAge: '1y', etag: false },
+));
+
 const client = new MongoClient(process.env.DATABASE_URL);
 client.connect();
 const db = client.db('vue_app');
@@ -68,6 +73,12 @@ app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
   res.json(populatedCart);
 });
 
-app.listen(8000, () => {
-  console.log('Server is listening on port 8000')
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log('Server is listening on port ' + port)
 });
